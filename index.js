75a63d07842883f2bab13a81ee11269e590cea43
Ranjit k <k.ranjitkumar1479@gmail.com>
2024-04-11 18:06:18 +0530

    added small details like using onkeydown event listener for more easier use

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-document.getElementById("button").addEventListener("click",()=>{
-    console.log("hi")
-    fetch("https://api.quotable.io/quotes/random").then((Response)=>{
-    return Response.json()
-}).then(data=>{
-    console.log("fetch")
-    quotes(data);
-}).catch(e=>{
-    alert("error",e)
-})});
-function quotes(array_data){
-    const data = array_data[0];
+function quotefetch(){
+    fetch("https://api.quotable.io/quotes/random")
+    .then((Response) => Response.json()).then((data)=>quotes(data[0]))
+    .catch(console.log("error"))
+}
+function quotes(data){
     document.getElementById("quote").innerText=data.content;
     document.getElementById("author").innerText=data.author;
 }
+addEventListener("keydown",(event)=>event.key=="Enter" && quotefetch())
+
+
